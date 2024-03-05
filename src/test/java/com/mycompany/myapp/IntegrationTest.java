41479ba18aabc9f1dfccdc64a39632dea0e0208d
tuannt21 <tuannt21@msb.com.vn>
2024-03-05 23:35:19 +0700

    init

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.config.AsyncSyncConfiguration;
+import com.mycompany.myapp.config.EmbeddedRedis;
+import com.mycompany.myapp.config.EmbeddedSQL;
+import com.mycompany.myapp.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { JdemoApp.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedRedis
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
